0149d71f708f000600a5e9f2c27e12052b26857e
sg-mvenkate <mvenkate@sgintl.com>
2022-11-30 21:05:10 +0530

    Quarkus application with Okta

---
+++ src/main/java/org/tns/GreetingResource.java
@@ -0,0 +1,19 @@
+package org.tns;
+
+import io.quarkus.security.Authenticated;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Authenticated
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
